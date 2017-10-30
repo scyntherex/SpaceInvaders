ab526337fe35d0de7346a0a3b56938c47606c274
Rone <scyntherex@Rones-MacBook-Pro.local>
2017-10-30 14:56:03 -0700

    Added Shot.java

--- src/spaceinv/Shot.java
+++ src/spaceinv/Shot.java
@@ -1,5 +1,27 @@
 package spaceinv;
 
-public class Shot {
+import javax.swing.ImageIcon;
 
+public class Shot extends Sprite {
+	
+	private final String shotImg = "src/images/shot.jpg";
+    private final int H_SPACE = 6;
+    private final int V_SPACE = 1;
+
+    public Shot() {
+    }
+
+    public Shot(int x, int y) {
+
+        initShot(x, y);
+    }
+
+    private void initShot(int x, int y) {
+
+        ImageIcon ii = new ImageIcon(shotImg);
+        setImage(ii.getImage());
+        
+        setX(x + H_SPACE);
+        setY(y - V_SPACE);
+    }
 }
